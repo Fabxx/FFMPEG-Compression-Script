7e5b73094ddc40546f9e9c60d3da8908d797be13
Fabx <30447649+Fabxx@users.noreply.github.com>
2025-03-29 11:13:09 +0100

    Update encScript.sh

--- encScript.sh
+++ encScript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Controllo dipendenze
 type zenity &>/dev/null
 zenityPresent=$?
 
@@ -17,7 +16,6 @@ if [ $parallelPresent -ne 0 ]; then
     exit 1
 fi
 
-# Selezione cartella
 path=$(zenity --title="Select the folder containing the videos" --directory --file-selection)
 
 if [[ -z "$path" ]]; then
@@ -37,13 +35,12 @@ for file in "$path"/*; do
     extension="${filename##*.}"
     filename_noext="${filename%.*}"
 
-    # Ignora i file con estensione .webm
+    # Ignore already converted files
     if [[ "$extension" == "webm" ]]; then
         echo "Skipping $file (already .webm)"
         continue
     fi
 
-    # Segmentazione video
     ffmpeg -i "$file" -c copy -segment_time 30 -f segment -reset_timestamps 1 "${path}/${filename_noext}"_%04d."$extension"
 
     if [[ ! -e "${path}/${filename_noext}_0000.$extension" ]]; then
@@ -51,16 +48,13 @@ for file in "$path"/*; do
         continue
     fi
 
-    # Conversione segmenti in WebM
+    # Webm conversion, handle special chars in files
     parallel -j 8 ffmpeg -i "{}" -c:v libvpx-vp9 -b:v 0 -crf 40 -c:a libopus -ac 2 \
         -threads 16 -row-mt 1 -cpu-used 8 -tile-columns 4 -frame-parallel 1 "{}.webm" ::: "${path}/${filename_noext}"_*."$extension"
 
-    # Creazione lista file
     find "$path" -maxdepth 1 -name "${filename_noext}_*.webm" -print0 | sort -zV | awk -v ORS='' '{print "file \x27" $0 "\x27\n"}' > file_list.txt
 
-    # Concatenazione file WebM
     ffmpeg -f concat -safe 0 -i file_list.txt -c copy "$path/out/${filename_noext}.webm"
 
-    # Pulizia file temporanei
     rm "${path}/${filename_noext}"_*."$extension" "${path}/${filename_noext}"_*.webm file_list.txt
 done
